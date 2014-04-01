cc9b02ecc4bb3286471df97f5ec237a6f2c84ad0
Eric Larchevêque <elarch@gmail.com>
2014-04-01 16:28:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BitID demonstration
+
+Source code for a Ruby on Rails implementation of the server side BitID protocol.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BitidDemo::Application.config.secret_token = '2d15e3faf821cbb7f13009667bd900c83e922dccfc2b92b97c3fa736378ecb537e13aabd51d9d5800f39d69a2983551906cae051f05c4d7bf2e212138dc44035'
